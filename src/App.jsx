8f1389874f4ade488a00ffc3ff49f0248aad9305
erfa97 <erfa97@kali.erfa>
2022-12-26 14:47:58 -0300

    cuarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import axios from 'axios'
 import Weather from './components/Weather'
-axios
+import Loading from './components/Loading'
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
 
+    <div className="containerLoading">
+
+    <Loading/>
+    </div>
+
+    <br /><br />
+
     <div className="container">
 
     <Weather/>
+    
     </div>
     
 
---
+++ src/components/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="containerCubo">
+      <div className="cubo">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="loading">
+        <div>
+          <h1>loading...</h1>
+       
+        </div>
+      </div>
+
+    </div>
+    );
+};
+
+export default Loading;
